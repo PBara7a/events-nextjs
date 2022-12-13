86c49d954ebe3d589f231c4590d55eb3f38c0018
Paulo Barata <pbara7a@gmail.com>
2022-12-13 18:45:18 +0000

    All events page data fetched from firebase

--- pages/events/index.tsx
+++ pages/events/index.tsx
@@ -1,17 +1,30 @@
 import EventList from "../../components/events/EventList";
 import EventSearch from "../../components/events/EventSearch";
-import { getAllEvents } from "../../dummy-data";
+import { getAllEvents } from "../../utilities/apiUtils";
 import { Event } from "../../types";
 
-const events: Array<Event> = getAllEvents();
+type EventsPageProps = {
+  events: Array<Event>;
+};
 
-function EventsPage() {
+function EventsPage({ events }: EventsPageProps) {
   return (
     <>
       <EventSearch />
       <EventList events={events} />
     </>
   );
+}
+
+export async function getStaticProps() {
+  const events = await getAllEvents();
+
+  return {
+    props: {
+      events,
+    },
+    revalidate: 60,
+  };
 }
 
 export default EventsPage;
